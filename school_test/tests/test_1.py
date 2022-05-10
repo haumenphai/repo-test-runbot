20762f85b315442c4ccb68b834184be93994912a
Trinh Duc Do <codegunpow@gmail.com>
2022-05-10 15:44:35 +0700

    remove test fAILED

--- school_test/tests/test_1.py
+++ school_test/tests/test_1.py
@@ -8,5 +8,5 @@ class Test1(SavepointCase):
         student1 = self.env['student.student'].create({'name': 'Student 1'})
         self.assertEqual(student1.name, 'Student 1')
 
-    def test_02_example_run_test_tag_failed(self):
-        assert 1 + 1 == 3
+    # def test_02_example_run_test_tag_failed(self):
+    #     assert 1 + 1 == 3
