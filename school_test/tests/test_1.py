96003f71b80f7cb6e28d6dd981e119351006eedf
Trinh Duc Do <codegunpow@gmail.com>
2022-05-11 10:34:49 +0700

    [TEST FAILED]

--- school_test/tests/test_1.py
+++ school_test/tests/test_1.py
@@ -6,4 +6,4 @@ class Test1(SavepointCase):
 
     def test_01_example_run_test_tag(self):
         student1 = self.env['student.student'].create({'name': 'Student 1'})
-        self.assertEqual(student1.name, 'Student 1')
+        self.assertEqual(student1.name, 'Student 2')
